174753241530351092ba3c4fd0239fb09d092f91
Abdullah Al Zayed <rudro54@gmail.com>
2024-07-06 15:12:10 +0600

    Prime Count added

---
+++ PrimeCount.java
@@ -0,0 +1,44 @@
+public class PrimeCount {
+
+    public static int PrimeCountMethod(int start, int end) {
+
+        if (start > end) {
+            return 0;
+        }
+
+        int counter = 0;
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                counter++;
+            }
+        }
+
+        return counter;
+
+    }
+
+    public static boolean isPrime(int n) {
+        if (n <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(PrimeCountMethod(10, 30));
+        System.out.println(PrimeCountMethod(11, 29));
+        System.out.println(PrimeCountMethod(20, 22));
+        System.out.println(PrimeCountMethod(1, 1));
+        System.out.println(PrimeCountMethod(5, 5));
+        System.out.println(PrimeCountMethod(6, 2));
+        System.out.println(PrimeCountMethod(-10, 6));
+
+    }
+
+}
