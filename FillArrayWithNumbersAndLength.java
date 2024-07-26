5c7bbc0609aec35fed051b94b35e3bdaa09baf7c
Abdullah Al Zayed <rudro54@gmail.com>
2024-07-26 21:41:08 +0600

    fill funciton added

--- FillArrayWithNumbersAndLength.java
+++ FillArrayWithNumbersAndLength.java
@@ -17,6 +17,20 @@ public class FillArrayWithNumbersAndLength {
 
     }
 
+    public static int[] fill(int[] arr, int k, int n) {
+        if (k <= 0 || n <= 0) {
+            return null;
+        }
+
+        int[] result = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            result[i] = arr[i % k];
+        }
+
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println(
                 Arrays.toString(FillArrayWithNumbersAndLengthMethod(new int[] { 1, 2, 3, 5, 9, 12, -2, -1 }, 3, 10)));
@@ -24,6 +38,16 @@ public class FillArrayWithNumbersAndLength {
                 Arrays.toString(FillArrayWithNumbersAndLengthMethod(new int[] { 4, 2, -3, 12 }, 1, 5)));
         System.out.println(
                 Arrays.toString(FillArrayWithNumbersAndLengthMethod(new int[] { 2, 6, 9, 0, -3 }, 0, 4)));
+
+        System.out.println("trying new function .....");
+
+        System.out.println(
+                Arrays.toString(fill(new int[] { 1, 2, 3, 5, 9, 12, -2, -1 }, 3, 10)));
+        System.out.println(
+                Arrays.toString(fill(new int[] { 4, 2, -3, 12 }, 1, 5)));
+        System.out.println(
+                Arrays.toString(fill(new int[] { 2, 6, 9, 0, -3 }, 0, 4)));
+
     }
 
 }
