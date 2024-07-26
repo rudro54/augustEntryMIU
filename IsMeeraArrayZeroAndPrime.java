ce88c2f22679243fd410ba183403402fd13f5949
Abdullah Al Zayed <rudro54@gmail.com>
2024-07-26 18:40:10 +0600

    isMeera added second time

--- IsMeeraArrayZeroAndPrime.java
+++ IsMeeraArrayZeroAndPrime.java
@@ -31,6 +31,48 @@ public class IsMeeraArrayZeroAndPrime {
 
     }
 
+    public static int isPrimeNumber(int n) {
+        if (n <= 1) {
+            return 0;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % 2 == 0) {
+
+                return 0;
+            }
+        }
+
+        return 1;
+    }
+
+    public static int isMeera(int[] a) {
+
+        boolean hasZero = false;
+        boolean hasPrime = false;
+
+        for (int num : a) {
+            if (num == 0) {
+                hasZero = true;
+            }
+
+            if (isPrimeNumber(num) == 1) {
+                hasPrime = true;
+            }
+        }
+
+        if (hasZero && hasPrime) {
+            return 1;
+        }
+
+        if (!hasZero && !hasPrime) {
+            return 1;
+        }
+
+        return 0;
+
+    }
+
     public static void main(String[] args) {
 
         System.out.println(IsMeeraArrayZeroAndPrimeMethod(new int[] { 7, 6, 0, 10, 1 }));
@@ -42,6 +84,18 @@ public class IsMeeraArrayZeroAndPrime {
         System.out.println(IsMeeraArrayZeroAndPrimeMethod(new int[] { 6, 10, 0 }));
 
         System.out.println(IsMeeraArrayZeroAndPrimeMethod(new int[] { 3, 7, 0, 8, 0, 5 }));
+
+        System.out.println("checking again with a new function .... ");
+
+        System.out.println(isMeera(new int[] { 7, 6, 0, 10, 1 }));
+
+        System.out.println(isMeera(new int[] { 6, 10, 1 }));
+
+        System.out.println(isMeera(new int[] { 7, 6, 10 }));
+
+        System.out.println(isMeera(new int[] { 6, 10, 0 }));
+
+        System.out.println(isMeera(new int[] { 3, 7, 0, 8, 0, 5 }));
 
     }
 
