9ebd8e6bb104e63493df28ea03941128ca0e8fae
Abdullah Al Zayed <rudro54@gmail.com>
2024-10-07 14:14:49 -0400

    october entry question added

--- QuestionOne.java
+++ QuestionOne.java
@@ -19,6 +19,16 @@ public class QuestionOne {
         return 2 * evenSum + 3 * oddSum;
     }
 
+    public static int computeWeightedSumTwo(int[] a) {
+        int sum = 0;
+
+        for (int num : a) {
+            sum = (num % 2 == 0) ? (sum + num * 2) : (sum + num * 3);
+        }
+
+        return sum;
+    }
+
     public static void main(String[] args) {
         System.out.println(computeWeightedSum(new int[] { 1, 2, 3, 4, 5 }));
         System.out.println(computeWeightedSum(new int[] { 1, 3, 5 }));
@@ -27,6 +37,16 @@ public class QuestionOne {
         System.out.println(computeWeightedSum(new int[] { 2 }));
         System.out.println(computeWeightedSum(new int[] { 0, 0, 0, 0, 0 }));
         System.out.println(computeWeightedSum(new int[] {}));
+
+        System.out.println("new line");
+
+        System.out.println(computeWeightedSumTwo(new int[] { 1, 2, 3, 4, 5 }));
+        System.out.println(computeWeightedSumTwo(new int[] { 1, 3, 5 }));
+        System.out.println(computeWeightedSumTwo(new int[] { 2, 4, 6 }));
+        System.out.println(computeWeightedSumTwo(new int[] { 1 }));
+        System.out.println(computeWeightedSumTwo(new int[] { 2 }));
+        System.out.println(computeWeightedSumTwo(new int[] { 0, 0, 0, 0, 0 }));
+        System.out.println(computeWeightedSumTwo(new int[] {}));
 
     }
 
