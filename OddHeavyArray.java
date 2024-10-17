57b5ffaf38bc2c7e92f3f8eabf7b8a757704a041
Abdullah Al Zayed <rudro54@gmail.com>
2024-10-17 17:23:57 -0400

    october entry question added

---
+++ OddHeavyArray.java
@@ -0,0 +1,54 @@
+public class OddHeavyArray {
+
+    public static int isOddHeavy(int[] a) {
+
+        if (a == null || a.length == 0) {
+            return 0;
+        }
+
+        boolean hasEven = false;
+        boolean hasOdd = false;
+
+        int maxEven = Integer.MIN_VALUE;
+        int minOdd = Integer.MAX_VALUE;
+
+        for (int num : a) {
+
+            if (num % 2 == 0) {
+                hasEven = true;
+                if (num > maxEven) {
+                    maxEven = num;
+                }
+            } else {
+                hasOdd = true;
+                if (num < minOdd) {
+                    minOdd = num;
+                }
+            }
+
+        }
+
+        if (!hasOdd) {
+            return 0;
+
+        }
+
+        if (!hasEven || minOdd > maxEven) {
+            return 1;
+        }
+
+        return 0;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isOddHeavy(new int[] { 11, 4, 9, 2, 8 }));
+        System.out.println(isOddHeavy(new int[] { 11, 4, 9, 2, 3, 10 }));
+        System.out.println(isOddHeavy(new int[] { 1 }));
+        System.out.println(isOddHeavy(new int[] { 2 }));
+        System.out.println(isOddHeavy(new int[] { 1, 1, 1, 1 }));
+        System.out.println(isOddHeavy(new int[] { 2, 4, 6, 8, 11 }));
+        System.out.println(isOddHeavy(new int[] { -2, -4, -6, -8, -11 }));
+
+    }
+}
