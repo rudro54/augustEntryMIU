70c1e0509f6499e315e388579937ba7ec3741635
Abdullah Al Zayed <rudro54@gmail.com>
2024-10-24 14:27:06 -0400

    october entry question addedclear

---
+++ LayeredArray.java
@@ -0,0 +1,44 @@
+public class LayeredArray {
+
+    public static int isLayered(int[] a) {
+
+        if (a.length < 2) {
+            return 0;
+        }
+        int counter = 1;
+
+        for (int i = 1; i < a.length; i++) {
+
+            if (a[i] < a[i - 1]) {
+                return 0;
+
+            }
+
+            if (a[i] == a[i - 1]) {
+                counter++;
+
+            } else {
+                if (counter < 2) {
+                    return 0;
+                }
+                counter = 1;
+            }
+
+        }
+        if (counter < 2) {
+            return 0;
+        }
+        return 1;
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isLayered(new int[] { 1, 1, 2, 2, 2, 3, 3 }));
+        System.out.println(isLayered(new int[] { 3, 3, 3, 3, 3, 3, 3 }));
+        System.out.println(isLayered(new int[] { 1, 2, 2, 2, 3, 3 }));
+        System.out.println(isLayered(new int[] { 2, 2, 2, 3, 3, 1, 1, }));
+        System.out.println(isLayered(new int[] { 2 }));
+        System.out.println(isLayered(new int[] {}));
+    }
+
+}
