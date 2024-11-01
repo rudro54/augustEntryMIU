e264fe22db2fdc07371e24fb8eb9312a9629a267
Abdullah Al Zayed <rudro54@gmail.com>
2024-10-31 22:38:51 -0500

    october entry question added

--- isRapidlyIncreasing.java
+++ isRapidlyIncreasing.java
@@ -19,6 +19,29 @@ public class isRapidlyIncreasing {
 
     }
 
+    public static int isRapidlyIncreasing(int[] a) {
+
+        if (a == null || a.length == 0) {
+            return 0;
+        }
+
+        int sum = a[0];
+
+        for (int i = 1; i < a.length; i++) {
+            int firstElement = a[i];
+
+            if (firstElement > sum * 2) {
+                sum += firstElement;
+            } else {
+                return 0;
+            }
+
+        }
+
+        return 1;
+
+    }
+
     public static void main(String[] args) {
 
         System.out.println(isRapidlyIncreasingMethod(new int[] { 1, 3, 9, 27 }));
@@ -27,6 +50,14 @@ public class isRapidlyIncreasing {
         System.out.println(isRapidlyIncreasingMethod(new int[] { 1, 3, 9, 26 }));
         System.out.println(isRapidlyIncreasingMethod(new int[] { 1, 3, 7, 26 }));
         System.out.println(isRapidlyIncreasingMethod(new int[] { 1, 3, 8, 27 }));
+        System.out.println("new line");
+        System.out.println("new line");
+        System.out.println(isRapidlyIncreasing(new int[] { 1, 3, 9, 27 }));
+        System.out.println(isRapidlyIncreasing(new int[] { 1, 3, 200, 500 }));
+        System.out.println(isRapidlyIncreasing(new int[] { 1 }));
+        System.out.println(isRapidlyIncreasing(new int[] { 1, 3, 9, 26 }));
+        System.out.println(isRapidlyIncreasing(new int[] { 1, 3, 7, 26 }));
+        System.out.println(isRapidlyIncreasing(new int[] { 1, 3, 8, 27 }));
 
     }
 
