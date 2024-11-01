812fe65d37a4519623177c638cbe15ac4d220b38
Abdullah Al Zayed <rudro54@gmail.com>
2024-10-31 23:13:01 -0500

    october entry question added

--- CharacterArray.java
+++ CharacterArray.java
@@ -16,6 +16,20 @@ public class CharacterArray {
 
     }
 
+    public static char[] CharacterArrayWay(char[] arr, int start, int length) {
+        if (arr == null || start < 0 || length < 0 || start + length > arr.length) {
+            return null;
+        }
+
+        char[] resultArray = new char[length];
+
+        for (int i = 0; i < length; i++) {
+            resultArray[i] = arr[start + i];
+        }
+
+        return resultArray;
+    }
+
     public static void main(String[] args) {
         System.out.println(java.util.Arrays.toString(CharacterArrayMethod(new char[] { 'a', 'b', 'c' }, 0, 4)));
         System.out.println(java.util.Arrays.toString(CharacterArrayMethod(new char[] { 'a', 'b', 'c' }, 0, 3)));
@@ -34,6 +48,27 @@ public class CharacterArray {
         System.out.println(java.util.Arrays.toString(CharacterArrayMethod(new char[] { 'a', 'b', 'c' }, -1, 2)));
         System.out.println(java.util.Arrays.toString(CharacterArrayMethod(new char[] { 'a', 'b', 'c' }, -1, -2)));
         System.out.println(java.util.Arrays.toString(CharacterArrayMethod(new char[] {}, 0, 1)));
+
+        System.out.println("new line");
+        System.out.println("new line");
+
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 0, 4)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 0, 3)));
+
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 0, 2)));
+
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 0, 1)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 1, 3)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 1, 2)));
+
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 1, 1)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 2, 2)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 2, 1)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 3, 1)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, 1, 0)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, -1, 2)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] { 'a', 'b', 'c' }, -1, -2)));
+        System.out.println(java.util.Arrays.toString(CharacterArrayWay(new char[] {}, 0, 1)));
 
     }
 }
