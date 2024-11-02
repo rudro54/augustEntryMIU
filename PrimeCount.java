3c88f948a7460d4119438e826f615dc6abb0a6cc
Abdullah Al Zayed <rudro54@gmail.com>
2024-11-01 23:19:09 -0500

    october entry question added

--- PrimeCount.java
+++ PrimeCount.java
@@ -34,6 +34,36 @@ public class PrimeCount {
         return true;
     }
 
+    public static boolean isPrimeWay(int n) {
+        if (n <= 1) {
+            return false;
+        }
+
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static int countPrimeWay(int start, int end) {
+        if (start > end) {
+            return 0;
+        }
+
+        int counter = 0;
+
+        for (int i = start; i <= end; i++) {
+            if (isPrime(i)) {
+                counter++;
+            }
+        }
+
+        return counter;
+    }
+
     public static void main(String[] args) {
         System.out.println(PrimeCountMethod(10, 30));
         System.out.println(PrimeCountMethod(11, 29));
@@ -42,6 +72,17 @@ public class PrimeCount {
         System.out.println(PrimeCountMethod(5, 5));
         System.out.println(PrimeCountMethod(6, 2));
         System.out.println(PrimeCountMethod(-10, 6));
+
+        System.out.println("new line");
+        System.out.println("new line");
+
+        System.out.println(countPrimeWay(10, 30));
+        System.out.println(countPrimeWay(11, 29));
+        System.out.println(countPrimeWay(20, 22));
+        System.out.println(countPrimeWay(1, 1));
+        System.out.println(countPrimeWay(5, 5));
+        System.out.println(countPrimeWay(6, 2));
+        System.out.println(countPrimeWay(-10, 6));
 
     }
 
