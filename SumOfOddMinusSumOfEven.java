da5da4bebdb2ddffb3ff0b0c38c4dc304b5d511b
Abdullah Al Zayed <rudro54@gmail.com>
2024-11-01 20:39:09 -0500

    october entry question added

--- SumOfOddMinusSumOfEven.java
+++ SumOfOddMinusSumOfEven.java
@@ -16,6 +16,30 @@ public class SumOfOddMinusSumOfEven {
 
     }
 
+    public static int fWay(int[] a) {
+
+        if (a == null || a.length == 0) {
+            return 0;
+        }
+
+        int oddSum = 0;
+        int evenSum = 0;
+
+        for (int num : a) {
+
+            if (num % 2 != 0) {
+                oddSum += num;
+            }
+            if (num % 2 == 0) {
+                evenSum += num;
+            }
+
+        }
+
+        return oddSum - evenSum;
+
+    }
+
     public static void main(String[] args) {
         System.out.println(f(new int[] { 1 }));
         System.out.println(f(new int[] { 1, 2 }));
@@ -26,6 +50,17 @@ public class SumOfOddMinusSumOfEven {
         System.out.println(f(new int[] { 4, 1, 2, 3 }));
         System.out.println(f(new int[] { 1, 1 }));
         System.out.println(f(new int[] {}));
+        System.out.println("New Line");
+        System.out.println("New Line");
+        System.out.println(fWay(new int[] { 1 }));
+        System.out.println(fWay(new int[] { 1, 2 }));
+        System.out.println(fWay(new int[] { 1, 2, 3 }));
+        System.out.println(fWay(new int[] { 1, 2, 3, 4 }));
+        System.out.println(fWay(new int[] { 3, 3, 4, 4 }));
+        System.out.println(fWay(new int[] { 3, 2, 3, 4 }));
+        System.out.println(fWay(new int[] { 4, 1, 2, 3 }));
+        System.out.println(fWay(new int[] { 1, 1 }));
+        System.out.println(fWay(new int[] {}));
 
     }
 
