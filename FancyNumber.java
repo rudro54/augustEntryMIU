ed59fbf27e21f4bd0d96fb264e7b83c8611298e7
Abdullah Al Zayed <rudro54@gmail.com>
2024-11-02 00:29:15 -0500

    october entry question added

--- FancyNumber.java
+++ FancyNumber.java
@@ -25,12 +25,42 @@ public class FancyNumber {
 
     }
 
+    public static int isFancyWay(int n) {
+
+        if (n == 1) {
+            return 1;
+        }
+        int prevOne = 1;
+        int prevTwo = 1;
+        int currentFancy = 0;
+
+        while (currentFancy < n) {
+            currentFancy = 3 * prevOne + 2 * prevTwo;
+
+            if (currentFancy == n) {
+                return 1;
+            }
+            prevTwo = prevOne;
+            prevOne = currentFancy;
+        }
+
+        return 0;
+
+    }
+
     public static void main(String[] args) {
         System.out.println(isFancy(1));
         System.out.println(isFancy(5));
         System.out.println(isFancy(17));
         System.out.println(isFancy(61));
         System.out.println(isFancy(15));
+        System.out.println("new line");
+        System.out.println("new line");
+        System.out.println(isFancyWay(1));
+        System.out.println(isFancyWay(5));
+        System.out.println(isFancyWay(17));
+        System.out.println(isFancyWay(61));
+        System.out.println(isFancyWay(15));
     }
 
 }
