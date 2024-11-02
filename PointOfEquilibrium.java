5e8791705e60a279b53166f1b42b37aee00edc53
Abdullah Al Zayed <rudro54@gmail.com>
2024-11-01 22:17:28 -0500

    october entry question added

--- PointOfEquilibrium.java
+++ PointOfEquilibrium.java
@@ -45,6 +45,29 @@ public class PointOfEquilibrium {
         return -1;
     }
 
+    public static int poeWay(int[] a) {
+
+        if (a == null || a.length == 0) {
+            return -1;
+        }
+        int sum = 0;
+        for (int num : a) {
+            sum += num;
+        }
+
+        int leftSum = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            sum -= a[i];
+            if (leftSum == sum) {
+                return i;
+            }
+            leftSum += a[i];
+
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println(PointOfEquilibriumMethod(new int[] { 1, 8, 3, 7, 10, 2 }));
         System.out.println(PointOfEquilibriumMethod(new int[] { 1, 5, 3, 1, 1, 1, 1, 1, 1 }));
@@ -60,6 +83,14 @@ public class PointOfEquilibrium {
         System.out.println(PointOfEquilibriumWay(new int[] { 1, 2, 3 }));
         System.out.println(PointOfEquilibriumWay(new int[] { 3, 4, 5, 10 }));
         System.out.println(PointOfEquilibriumWay(new int[] { 1, 2, 10, 3, 4 }));
+        System.out.println("new line");
+        System.out.println("new line");
+        System.out.println(poeWay(new int[] { 1, 8, 3, 7, 10, 2 }));
+        System.out.println(poeWay(new int[] { 1, 5, 3, 1, 1, 1, 1, 1, 1 }));
+        System.out.println(poeWay(new int[] { 2, 1, 1, 1, 2, 1, 7 }));
+        System.out.println(poeWay(new int[] { 1, 2, 3 }));
+        System.out.println(poeWay(new int[] { 3, 4, 5, 10 }));
+        System.out.println(poeWay(new int[] { 1, 2, 10, 3, 4 }));
     }
 
 }
