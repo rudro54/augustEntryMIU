7ffbe61816a61986275c83b59d85d19e775ac833
Abdullah Al Zayed <rudro54@gmail.com>
2024-11-01 20:25:57 -0500

    october entry question added

--- PointOfEquilibrium.java
+++ PointOfEquilibrium.java
@@ -25,6 +25,26 @@ public class PointOfEquilibrium {
 
     }
 
+    public static int PointOfEquilibrium(int[] a) {
+
+        int leftSum = 0;
+        int totalSum = 0;
+
+        for (int num : a) {
+            totalSum += num;
+        }
+
+        for (int i = 0; i < a.length; i++) {
+            totalSum -= a[i];
+            if (leftSum == totalSum) {
+                return i;
+            }
+            leftSum += a[i];
+        }
+
+        return -1;
+    }
+
     public static void main(String[] args) {
         System.out.println(PointOfEquilibriumMethod(new int[] { 1, 8, 3, 7, 10, 2 }));
         System.out.println(PointOfEquilibriumMethod(new int[] { 1, 5, 3, 1, 1, 1, 1, 1, 1 }));
@@ -32,6 +52,14 @@ public class PointOfEquilibrium {
         System.out.println(PointOfEquilibriumMethod(new int[] { 1, 2, 3 }));
         System.out.println(PointOfEquilibriumMethod(new int[] { 3, 4, 5, 10 }));
         System.out.println(PointOfEquilibriumMethod(new int[] { 1, 2, 10, 3, 4 }));
+        System.out.println("new line");
+        System.out.println("new line");
+        System.out.println(PointOfEquilibrium(new int[] { 1, 8, 3, 7, 10, 2 }));
+        System.out.println(PointOfEquilibrium(new int[] { 1, 5, 3, 1, 1, 1, 1, 1, 1 }));
+        System.out.println(PointOfEquilibrium(new int[] { 2, 1, 1, 1, 2, 1, 7 }));
+        System.out.println(PointOfEquilibrium(new int[] { 1, 2, 3 }));
+        System.out.println(PointOfEquilibrium(new int[] { 3, 4, 5, 10 }));
+        System.out.println(PointOfEquilibrium(new int[] { 1, 2, 10, 3, 4 }));
     }
 
 }
