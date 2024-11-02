7a791780715b4b49f439f4a7a2fc2a4c8fd4120b
Abdullah Al Zayed <rudro54@gmail.com>
2024-11-01 21:22:12 -0500

    october entry question added

--- ReverseANumber.java
+++ ReverseANumber.java
@@ -22,6 +22,30 @@ public class ReverseANumber {
 
     }
 
+    public static int reverseWay(int n) {
+
+        boolean isNegative = n < 0;
+
+        if (isNegative) {
+            n = -n;
+        }
+
+        int reversed = 0;
+
+        while (n != 0) {
+            int digit = n % 10;
+            reversed = reversed * 10 + digit;
+            n /= 10;
+        }
+
+        if (isNegative) {
+            reversed = -reversed;
+        }
+
+        return reversed;
+
+    }
+
     public static int reversedMethod(int n) {
         int reversed = 0;
         boolean isNegative = n < 0;
@@ -58,6 +82,14 @@ public class ReverseANumber {
         System.out.println(reversedMethod(1000));
         System.out.println(reversedMethod(0));
         System.out.println(reversedMethod(-12345));
+        System.out.println("new line");
+        System.out.println("new line");
+        System.out.println(reverseWay(1234));
+        System.out.println(reverseWay(12005));
+        System.out.println(reverseWay(1));
+        System.out.println(reverseWay(1000));
+        System.out.println(reverseWay(0));
+        System.out.println(reverseWay(-12345));
 
     }
 
